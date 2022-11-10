871db58a3bf9a6ec286147d5aac3cee500f3ad54
julia berglund <jullano9@hotmail.com>
2022-11-10 17:56:47 +0100

    Now you can search for all the pokemons with their full name

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,6 @@ import Card from "./Components/Card";
 import Pokeinfo from "./Components/Pokeinfo";
 import RandomPokemon from "./Components/RandomPokemon";
 import axios from "axios";
-
 import { AiFillCloseSquare } from "react-icons/ai";
 <link
   rel="stylesheet"
@@ -35,21 +34,39 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
         const getPokemon = async (res) => {
             const array = []
             res.forEach(element => {
-        
             });
             for (let i = 0; i < res.length; i++) {
             const result = await axios.get(res[i].url)
             array.push(result.data)
             }
-        
             setPokeData(array);
             encounterWildPokemon(array);
         }
 
-        
+        const formatPoke = (res) => {
+            const data = [{...res}]
+            setFilteredData(data)
+        }
 
-        const handleSearch = (e) => {
-            setSearch(e.target.value);
+
+        //Så när du skriver i sökrutan försöker den kalla på aipet men om den retuernerar error så kommer catch(error) ta över.
+        const handleSearch =async (e) => {
+            try {
+                setSearch(e.target.value);
+                if (!e.target.value) {
+                    return setFilteredData(pokeData);
+                }
+                setLoading(true)
+                const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${e.target.value}/`)
+                formatPoke(res.data)
+                setLoading(false)
+                
+            } catch (error) {
+                setLoading(false)
+                if (error.response.status === 404) {
+                    filterPokemons()
+                }
+            }
         }
 
         //Sökfunktionen som sorterar pokemons namnen, tar fram den pokemon du sökt på.
@@ -104,17 +121,10 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
             pokeFun();
         }, [url])
 
-
-
       
-    const heya = () => {
-        // alert('called')
-        console.log(pokeDex)
-       if (pokeDex !== undefined && pokeDex != null) {
-        console.log('pig')
-        
+     const heya = () => {
+       if (pokeDex !== undefined && pokeDex != null) {        
         document.getElementById('blah').focus();
-
         document.getElementById('blah').style.display = 'inline-block';
         document.getElementById('inblah').style.display = 'block'
         document.getElementById('b').style.display = 'block';
@@ -123,7 +133,6 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
         
         
        } else if(pokeDex === null){
-        console.log('hrllo')
         document.getElementById('inblah').style.display = 'none';
         document.getElementById('blah').style.display = 'none';
         document.getElementById('b').style.display = 'none';
@@ -132,8 +141,6 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
     } 
 
         return (
-
-            
             <>  
             {heya()}
             <div className="scroll" style={{position: 'relative', pointerEvents: 'auto'}} id="full">
@@ -142,7 +149,7 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
             {/* -----------Om wildpokemon finns kan du klicka på bilden och får fram information om den pokemonen  */}
             {Object.values(wildPokemon).length > 0 && <RandomPokemon wildPokemon={wildPokemon} infoPokemon={poke => setPokeDex(poke)} />}
             
-            <button className='random-pokemon' onClick={()=>encounterWildPokemon(pokeData)}>Refresh</button>
+            {Object.values(wildPokemon).length > 0 && <button className='random-pokemon' onClick={()=>encounterWildPokemon(pokeData)}>Refresh</button>}
             { catchedPokemons.map((item) => {
                         
                 return (
@@ -160,7 +167,7 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
                 <div className="left-content">
                     <Card pokemon={filteredData.length > 0 ? filteredData : pokeData} loading={loading} infoPokemon={poke => setPokeDex(poke)} />
 
-                    <div className="btn-group">
+                   {(filteredData.length > 0 || pokeData.length > 0 ) &&<div className="btn-group">
                         {prevUrl && <button onClick={() => {
                             setPokeData([])
                             setFilteredData([])
@@ -174,7 +181,7 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
                             setSearch('')
                             setUrl(nextUrl)
                         }}>Next</button>}
-                    </div>
+                    </div>} 
                 </div>
                 
                 <div style={{position: 'absolute',bottom: '50%',marginRight: 'auto',marginLeft: 'auto',width: 'auto', height: 'auto', backgroundColor: 'transparent',position:'fixed', padding: 20, top: '10%', left: '50%',transform: 'translate(-50%, 0)', pointerEvents: 'none'}} id="blah">
@@ -183,13 +190,9 @@ const Main = ({ catchedPokemons, setCatchedPokemons}) => {
                     {pokeDex && <AiFillCloseSquare onClick={() => 
                         {setPokeDex(null)}} size={30} style={{float: 'left'}} />
                         }</div>
-                     
                         <Pokeinfo catchPokemon = {catchPokemon} data={pokeDex} />
-                    
                     </div>
-                
                 </div>
-                
                 </div>
                 </div>
         </>
